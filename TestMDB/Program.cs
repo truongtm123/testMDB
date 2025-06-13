698807ae4300617f7db26b997f45c1f339f6471c
Trent <truong.ta@sotatek.com>
2025-06-13 14:00:11 +0700

    add code

--- TestMDB/Program.cs
+++ TestMDB/Program.cs
@@ -8,7 +8,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+
 var app = builder.Build();
+app.UseCors("AllowAll");
+
 Batteries.Init();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
